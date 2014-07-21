0cd9677e0b8c8a4c5ed3f863396db7f8f9c648fb
mrubino <mrubino@millennialmedia.com>
2014-07-21 07:27:39 -0400

    project template.

---
+++ src/com/mtrubs/kanban/MainActivity.java
@@ -0,0 +1,17 @@
+package com.mtrubs.kanban;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author mrubino
+ * @since 2014-07-20
+ */
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
